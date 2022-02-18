094328407f58678eaa0022a7ea19956d122ea53e
JBCodeBook <87623023+JBCodeBook@users.noreply.github.com>
2022-02-17 21:52:42 -0500

    Project is able to run and produce html files

--- main.py
+++ main.py
@@ -6,17 +6,11 @@ import watsonSTT
 
 def main():
     load_dotenv()
+
     API = os.getenv("API_KEY")
     URL = os.getenv("URL")
 
-    # watsonSTT.process_JSON(os.path.abspath("output.txt"))
-    # watsonSTT.print_to_html(os.path.abspath("newTranscript"))
-
-    tmp = watsonSTT.getFiles()
-    watsonSTT.watsonStart(API, URL, tmp[0])
-
-
-
+    watsonSTT.watsonStart(API, URL)
 
 if __name__ == "__main__":
     main()
--- watsonSTT.py
+++ watsonSTT.py
@@ -1,10 +1,16 @@
 import json
 import os
+import sys
+from colorama import init
+import subprocess
+from termcolor import cprint
+from pyfiglet import figlet_format
 from dotenv import load_dotenv
 from ibm_watson import SpeechToTextV1
 from ibm_watson.websocket import RecognizeCallback, AudioSource
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import subprocess
+
+init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
 
 
 def getFiles():
@@ -19,7 +25,7 @@ def getFiles():
                 print(str(count + 1) + opt)
                 count += 1
 
-            select = int(input("What type of file would you like to process : \n"))
+            select = int(input("\nWhat type of file would you like to process : \n"))
             if len(typeof_files) + 1 > select > 0:
                 file_name = typeof_files[select - 1]
                 print("\nselected : ", file_name, '\n')
@@ -33,105 +39,98 @@ def getFiles():
 
     for files in os.listdir(transcript_file):
         if os.path.splitext(files)[1] == file_name:
-            listof_files.append(files)
+            f = transcript_file + '\\' + files
+            listof_files.append(f)
 
     return listof_files
 
 
-def json_time(json_Object):
-    flag = 0
-    for i in json_Object['results']:
-        if (flag != 1):
-            time_start = (i['alternatives'][0]['timestamps'][0][1])
-            flag = 1
-        else:
-            time_end = (i['alternatives'][0]['timestamps'][0][1])
+def process_JSON(json_files):
+    tmp = os.listdir('./output')
+
+    for file in tmp:
+        # print(f"Processing {file} file...")
+        with open('.\\output\\' + file, "r") as read_file:
+            json_object = json.load(read_file)
+
+        transcript_dict = {}
+        count = 1
+
+        for i in json_object['results']:
+            time_start = i['alternatives'][0]['timestamps'][0][1]
+            for j in json_object['speaker_labels']:
+                if time_start == (j['from']):
+                    transcript_dict[count] = {'speaker': j['speaker'], 'confidence': i['alternatives'][0]['confidence'],
+                                              'transcript': i['alternatives'][0]['transcript']}
+                    count += 1
+
+            with open('.\\transcripts\\' + f'{file}_transcript.txt', 'w') as outFile:
+                json.dump(transcript_dict, outFile, indent=4)
 
 
-def process_JSON(json_file):
-    print("Processing JSON file...")
-    with open(json_file, "r") as read_file:
-
-        json_object = json.load(read_file)
-
-    transcript_dict = {}
-    count = 1
-
-    for i in json_object['results']:
-        time_start = i['alternatives'][0]['timestamps'][0][1]
-        for j in json_object['speaker_labels']:
-            if time_start == (j['from']):
-                transcript_dict[count] = {'speaker': j['speaker'], 'confidence': i['alternatives'][0]['confidence'],
-                                          'transcript': i['alternatives'][0]['transcript']}
-                count += 1
-
-    with open('newTranscript', 'w') as outFile:
-        json.dump(transcript_dict, outFile, indent=4)
-
-
-def print_to_html(json_file):
+def print_to_html():
     prev_speaker = None
     cur_speaker = None
 
-    if (os.path.basename('trasncript_HTML')):
-        os.remove(os.path.basename('transcript_HTML'))
+    if (os.path.isfile('trasncript_HTML.txt')):
+        os.remove(os.path.basename('transcript_HTML.txt'))
+    else:
+        print("file does not exist yet")
 
-    with open(json_file, "r") as read_file:
-        json_Object = json.load(read_file)
+    with open("transcript_HTML.txt", 'a+') as outFile:
+        for file in os.listdir('.\\transcripts\\'):
+            print("processing")
+            with open('.\\transcripts\\' + file, "r") as read_file:
+                json_Object = json.load(read_file)
 
-    for key in json_Object:
+            for key in json_Object:
 
-        cur_speaker = json_Object[key]['speaker']
-        transcript = json_Object[key]['transcript']
+                cur_speaker = json_Object[key]['speaker']
+                transcript = json_Object[key]['transcript']
 
-        with open("transcript_HTML", 'a+') as outFile:
-            if prev_speaker == cur_speaker:
-                line = " " * 5 + "<p>" + transcript + "</p>"
-                outFile.write(line + '\n')
+                if prev_speaker == cur_speaker:
+                    line = " " * 5 + "<p>" + transcript + "</p>"
+                    outFile.write(line + '\n')
 
-            elif prev_speaker == None or prev_speaker != cur_speaker:
-                line = "<h3>speaker 1</h3>"
-                outFile.write(line + '\n')
-                line = " " * 5 + "<p>" + transcript + "</p>"
-                outFile.write(line + '\n')
+                elif prev_speaker == None or prev_speaker != cur_speaker:
+                    line = "<h3>speaker 1</h3>"
+                    outFile.write(line + '\n')
+                    line = " " * 5 + "<p>" + transcript + "</p>"
+                    outFile.write(line + '\n')
 
-        prev_speaker = json_Object[key]['speaker']
-
-
-def watson_Compreess():
-    print("Compressing files")
-    command = 'ffmpeg -i 1.wav -vn -ar 44100 -ac 2 -b:a 192k audio.mp3'
-    subprocess.call(command, shell=True)
-    command = 'ffmpeg -i audio.mp3 -f segment -segment_time 360 -c copy %03d.mp3'
-    subprocess.call(command, shell=True)
-    print("Finished Compressing files")
+            prev_speaker = json_Object[key]['speaker']
 
 
 def listfolders():
-    filenames = os.listdir(".")  # get all files' and folders' names in the current directory
+    filenames = os.listdir("./audio")  # get all files' and folders' names in the current directory
+    filepath = os.path.join(os.path.abspath(".") + '\\audio\\')
 
     result = []
     ignore = ['.git', '.idea', '__pycache__']
     for filename in filenames:  # loop through all the files and folders
-        if (filename in ignore):
+
+        if filename in ignore:
+            print(f"ignoring file : {filename} \n")
             continue
-        if os.path.isdir(
-                os.path.join(os.path.abspath("."), filename)):  # check whether the current object is a folder or not
+
+        if os.path.isdir(filepath + filename):
+            print(f'{filename} added to the list \n')
             result.append(filename)
 
     result.sort()
-    print("test")
-
-    select = 0
-    acceptable_values = list(range(1, len(result)))
-    file_name = None
-
-    for i in range(0, len(result)):
-        print('[' + str((i + 1)) + ']', result[i])
 
     while True:
         try:
-            select = int(input("Which folder would you like to process : "))
+
+            cprint(figlet_format('Menu!', font='starwars'),
+                   'yellow', 'on_red', attrs=['bold'])
+
+            print("Please select Folder\n")
+
+            for i in range(0, len(result)):
+                print('[' + str((i + 1)) + ']', result[i])
+
+            select = int(input("\nWhich folder would you like to process : "))
             if len(result) + 1 > select > 0:
                 file_name = result[select - 1]
                 break
@@ -142,44 +141,62 @@ def listfolders():
         except ValueError:
             print("Value not accepted")
 
-    return os.path.abspath(file_name)
+    return os.path.abspath(filepath + file_name)
 
 
-# return error and repeat input
+def clean_folders():
+    pathToOutput = '.\\output'
+    pathToTranscripts = '.\\transcripts'
+
+    if(len(pathToOutput) > 0):
+        for f in os.listdir(pathToOutput):
+            print(f"Removing {os.path.join(pathToOutput, f)}")
+            os.remove(os.path.join(pathToOutput, f))
+
+    if(len(pathToTranscripts) > 0):
+        for f in os.listdir(pathToTranscripts):
+            print(f"Removing {os.path.join(pathToTranscripts, f)}")
+            os.remove(os.path.join(pathToTranscripts, f))
 
 
-def watsonStart(API, URL, fileto_convert):
+def watsonStart(API, URL):
+    output_path = os.path.abspath('output')
+    output_files = []
+    clean_folders()
 
-    print(fileto_convert)
+    filesto_convert = getFiles()
+
+    print("Authenticating service...")
 
     # Setup Service
-    # authenticator = IAMAuthenticator(API)
-    # service = SpeechToTextV1(authenticator=authenticator)
-    # service.set_service_url(URL)
-    #
-    # models = service.list_models().get_result()
-    #
-    # files = []
-    # for filename in os.listdir(path):
-    #     print(filename)
-    #     if filename.endswith(".wav") and filename != 'audio.mp3':
-    #         files.append(filename)
-    # files.sort()
-    #
-    # print("Starting to process audio")
-    #
-    # results = []
-    # fileName = path + "/" + files[0]
-    #
-    # # for filename in files:
-    # with open(fileName, 'rb') as f:
-    #     print(f)
-    #     res = service.recognize(
-    #         audio=f,
-    #         content_type='audio/wav',
-    #         timestamps=True,
-    #         speaker_labels=True
-    #     ).get_result()
-    #
-    #     with open('output.txt', 'w+') as out:
-    #         json.dumps(res, out)
+    try:
+        authenticator = IAMAuthenticator(API)
+        service = SpeechToTextV1(authenticator=authenticator)
+        service.set_service_url(URL)
+        print("Service Authenticated.")
+    except:
+        print("Could not Authenticate service")
+
+    models = service.list_models().get_result()
+
+    for file in filesto_convert:
+        file_basename = os.path.basename(file)
+        output_file = output_path + '\\' + os.path.basename(os.path.splitext(file)[0]) + ".txt"
+
+        output_files.append(output_file)
+        print(f"Transcribing {file_basename}")
+
+        with open(file, 'rb') as f:
+            res = service.recognize(
+                audio=f,
+                content_type='audio/wav',
+                timestamps=True,
+                speaker_labels=True
+            ).get_result()
+
+            with open(output_file, 'w+') as out:
+                print(f"Printing Json dump to file {output_file}")
+                json.dump(res, out)
+
+    process_JSON(output_files)
+    print_to_html()
